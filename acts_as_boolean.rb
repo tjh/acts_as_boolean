96828e5fd11ee0390d42c73f25f6fcdfab11794a
Tim Harvey <tim@literacy5.com>
2011-10-20 22:18:35 -0400

    Initial implementation with specs

--- acts_as_boolean.rb
+++ acts_as_boolean.rb
@@ -1,4 +1,13 @@
 module ActsAsBoolean
-  def acts_as_boolean(attributes)
+  def acts_as_boolean(*attributes)
+    [*attributes].each do |attribute|
+
+      # Define the = setter method
+      attr_writer attribute
+
+      # Define the ? accessor method
+      define_method("#{attribute}?") { !!instance_variable_get("@#{attribute}") }
+
+    end
   end
 end
--- acts_as_boolean_spec.rb
+++ acts_as_boolean_spec.rb
@@ -5,4 +5,60 @@ class ObjectWithActsAsBoolean
 end
 
 describe ActsAsBoolean do
+  let(:instance) { ObjectWithActsAsBoolean.new }
+
+
+  describe '#acts_as_boolean' do
+    context 'with an argument of :my_attribute' do
+      before(:each) { ObjectWithActsAsBoolean.send :acts_as_boolean, :my_attribute }
+
+      it 'should define a #my_attribute? accessor method' do
+        instance.respond_to?(:my_attribute?).should be_true
+      end
+
+      it 'should define an #my_attribute= setter method' do
+        instance.respond_to?(:my_attribute=).should be_true
+      end
+
+      it 'should not define a #my_attribute getter method' do
+        instance.respond_to?(:my_attribute).should be_false
+      end
+
+      it 'should return false when #my_attribute? is called and my_attribute is nil' do
+        instance.my_attribute?.should == false
+      end
+
+      it 'should return true when #my_attribute? is called and my_attribute is true' do
+        instance.my_attribute = true
+        instance.my_attribute?.should == true
+      end
+
+      it 'should return false when #my_attribute? is called and my_attribute is false' do
+        instance.my_attribute = false
+        instance.my_attribute?.should == false
+      end
+    end
+
+    context 'with two arguments, :my_attribute and :your_attribute' do
+      before(:each) { ObjectWithActsAsBoolean.send :acts_as_boolean, :my_attribute, :your_attribute }
+
+      it 'should define a #my_attribute? accessor method' do
+        instance.respond_to?(:my_attribute?).should be_true
+      end
+
+      it 'should define a #your_attribute? accessor method' do
+        instance.respond_to?(:your_attribute?).should be_true
+      end
+
+      it 'should return true when #your_attribute? is called and your_attribute is true' do
+        instance.your_attribute = true
+        instance.your_attribute?.should == true
+      end
+
+      it 'should return true when #my_attribute? is called and my_attribute is true' do
+        instance.my_attribute = true
+        instance.my_attribute?.should == true
+      end
+    end
+  end
 end
